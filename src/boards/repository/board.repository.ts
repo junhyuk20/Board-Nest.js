a6b46fbd3ae9213e3115dba4da49df591dda115a
junhyuk20 <kwk8002@naver.com>
2024-05-14 16:31:05 +0900

    오타 수정

--- src/boards/repository/board.repository.ts
+++ src/boards/repository/board.repository.ts
@@ -12,7 +12,7 @@ export class BoardRepository extends Repository<Board> {
 /**
  *  Repository 클래스 정의 
  * 
- *  1. 쿼리를 날릴 수 있는 함수들의 정의 되어 있다. Find entities, insert, update, delete, etc.
+ *  1. 쿼리를 날릴 수 있는 함수들이 정의 되어 있다. Find entities, insert, update, delete, etc.
  *  2. Repository를 상속 받았으므로 Repository 클래스안에 생성자가 정의되어 있다면 반드시 구현해 줘야 된다. 해당 부분 아홉번째 줄
  */
 

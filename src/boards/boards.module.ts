570961317de91e06203d5a11c9c11252278468c7
junhyuk20 <kwk8002@naver.com>
2024-05-20 08:21:00 +0900

    board.module.ts 주석 수정

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -8,7 +8,7 @@ import { Board } from './entity/board.entity';
 /** 
  * TypeOrmModule 클래스 속 메서드 정의 
  * 
- * 1. forRoot : 각 모듈에서 정의할 필요 없이 전역 모듈에서 설정 시 모든 모듈에서 사용 가능 ex) app.module.ts
+ * 1. forRoot : 각 모듈에서 정의할 필요 없이 전역 모듈에서 설정 시 모든 모듈에서 사용 가능 ex) 주로 메인이 되는 모듈인 app.module.ts 에서 사용
  * 2. forFeature : 특정 모듈에서만 사용할 때 ex) boards.module.ts 
  * 3. forRootAsync : 매번 다른 구성으로 여러 번 등록할 수 있는 모듈
 */
